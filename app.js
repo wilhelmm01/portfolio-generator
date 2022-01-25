4dfb4182e177f6e593ce14bf33a304175684c3ba
Your Name <matt14w@gmail.com>
2022-01-25 16:44:16 -0500

    answer validation

--- app.js
+++ app.js
@@ -1,14 +1,127 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
-const[name, github] = profileDataArgs;
-const FileSystem = require('fs')
-const generatePage = require("./src/page-template")
+const inquirer = require("inquirer");
+// const FileSystem = require('fs')
+// const generatePage = require("./src/page-template")
+
+// const pageHTML = generatePage(name, github)
+
+
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+      },
+      {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
+      }
+    ]);
+  };
+
+  const promptProject = portfolioData => {
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+      portfolioData.projects = [];
+    }
+    return inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is the name of your project? (Required)',
+          validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project name!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Provide a description of the project (Required)',
+          validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project description!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'checkbox',
+          name: 'languages',
+          message: 'What did you this project with? (Check all that apply)',
+          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+          type: 'input',
+          name: 'link',
+          message: 'Enter the GitHub link to your project. (Required)',
+          validate: linkInput => {
+            if (linkInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project GitHub link!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'confirm',
+          name: 'feature',
+          message: 'Would you like to feature this project?',
+          default: false
+        },
+        {
+          type: 'confirm',
+          name: 'confirmAddProject',
+          message: 'Would you like to enter another project?',
+          default: false
+        }
+      ])
+      .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
+  };
 
 
 
-  FileSystem.writeFile('index.html', generatePage(name,github), err =>{
-      if (err) throw err;
-      console.log('Portfolio Complete! Check out index.html to see output!')
-  })
+
+   
+  
+    promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+      console.log(portfolioData);
+    });
+
+
+//   FileSystem.writeFile('index.html', generatePage(name,github), err =>{
+//       if (err) throw err;
+//       console.log('Portfolio Complete! Check out index.html to see output!')
+//   })
 
 
 
@@ -23,15 +136,3 @@ const generatePage = require("./src/page-template")
 
 
 
-// const printProfileData = profileDataArr => {
-//     for (let i=0; i<profileDataArr.length; i++){
-//         console.log(profileDataArr[i])
-//     }
-
-//     console.log("===================");
-//     profileDataArr.forEach((profileItem) => {
-//         console.log(profileItem)
-//     });
-// };
-
-// printProfileData(profileDataArgs);
