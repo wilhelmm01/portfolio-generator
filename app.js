e35476e40421c395ae74bba9c7131ed40becf6f4
Your Name <matt14w@gmail.com>
2022-01-25 12:18:29 -0500

    fileshare

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const profileDataArgs = process.argv.slice(2,process.argv.length);
+const[name, github] = profileDataArgs;
+const FileSystem = require('fs')
+const generatePage = require("./src/page-template")
+
+
+
+  FileSystem.writeFile('index.html', generatePage(name,github), err =>{
+      if (err) throw err;
+      console.log('Portfolio Complete! Check out index.html to see output!')
+  })
 
 
 
 
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i<profileDataArr.length; i++){
-        console.log(profileDataArr[i])
-    }
 
-    console.log("===================");
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-};
 
-printProfileData(profileDataArgs);
+
+
+
+
+
+
+
+// const printProfileData = profileDataArr => {
+//     for (let i=0; i<profileDataArr.length; i++){
+//         console.log(profileDataArr[i])
+//     }
+
+//     console.log("===================");
+//     profileDataArr.forEach((profileItem) => {
+//         console.log(profileItem)
+//     });
+// };
+
+// printProfileData(profileDataArgs);
